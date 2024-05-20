883ef1717322d6fbe3ef583f82a86116a44cd98f
Hammad Ahmed <hammadak350@gmail.com>
2024-05-20 15:39:06 +0500

    Organized Code

--- lib/screens/update_events_screen.dart
+++ lib/screens/update_events_screen.dart
@@ -5,6 +5,7 @@ import '../utils/colors.dart';
 import '../utils/screen_utils.dart';
 import '../widgets/custom_textfield.dart';
 import '../widgets/custom_image_picker.dart';
+import '../widgets/custom_background.dart';
 
 class UpdateEventsScreen extends StatefulWidget {
   const UpdateEventsScreen({super.key});
@@ -36,102 +37,44 @@ class _UpdateEventsScreenState extends State<UpdateEventsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-
     return Scaffold(
       backgroundColor: appBarColor,
       body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              width: width,
-              height: height,
-              decoration: const BoxDecoration(
-                color: backgroundgrey,
-              ),
-            ),
-            Column(
+        child: CustomBackground(
+          title: 'Update Events',
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: ScreenUtils.width(context) * 0.05),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: height * 0.059, bottom: height * 0.035),
-                  child: Align(
-                    alignment: Alignment.topCenter,
-                    child: SizedBox(
-                      width: height * 0.134,
-                      height: height * 0.134,
-                      child: Image.asset(
-                        'assets/images/muet logo.png',
-                      ),
-                    ),
-                  ),
+                CustomImagePicker(
+                  initialImage: _image,
+                  onImagePicked: _pickImage,
                 ),
-                Expanded(
-                  child: Container(
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50.0),
-                        topRight: Radius.circular(50.0),
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                              top: height * 0.032, bottom: height * 0.045),
-                          child: Text(
-                            textAlign: TextAlign.center,
-                            'Update Events'.toUpperCase(),
-                            style: const TextStyle(
-                                fontSize: 20,
-                                color: Color(0xfffffc817),
-                                letterSpacing: 2,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: ScreenUtils.width(context) * 0.05),
-                          child: Column(
-                            children: [
-                              CustomImagePicker(
-                                initialImage: _image,
-                                onImagePicked: _pickImage,
-                              ),
-                              const SizedBox(height: 20,),
-                              CustomTextField(
-                                  hintText: "Event Title", controller: _titleController),
-                              SizedBox(
-                                height: ScreenUtils.height(context) * 0.03,
-                              ),
-                              CustomTextField(
-                                  hintText: "Event Description", controller: _descriptionController),
-                              SizedBox(
-                                height: ScreenUtils.height(context) * 0.03,
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Handle form submission
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: darkBlueColor,
-                                  padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                                ),
-                                child: const Text('Update Events', style: TextStyle(color: Colors.white),),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
+                const SizedBox(height: 20,),
+                CustomTextField(
+                    hintText: "Event Title", controller: _titleController),
+                SizedBox(
+                  height: ScreenUtils.height(context) * 0.03,
+                ),
+                CustomTextField(
+                    hintText: "Event Description", controller: _descriptionController),
+                SizedBox(
+                  height: ScreenUtils.height(context) * 0.03,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle form submission
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: darkBlueColor,
+                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
                   ),
-                )
+                  child: const Text('Update Events', style: TextStyle(color: Colors.white),),
+                ),
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
--- lib/widgets/custom_background.dart
+++ lib/widgets/custom_background.dart
@@ -1,55 +1,76 @@
-//TODO: Organize background code to reduce redundancy.
-// import 'package:flutter/material.dart';
-//
-// import '../utils/colors.dart';
-//       child: Stack(children: [
-//     Container(
-//       width: width,
-//       height: height,
-//       decoration: const BoxDecoration(
-//         color: backgroundgrey,
-//       ),
-//     ),
-//     Column(children: [
-//       Padding(
-//         padding: EdgeInsets.only(top: height * 0.059, bottom: height * 0.035),
-//         child: Align(
-//           alignment: Alignment.topCenter,
-//           child: SizedBox(
-//             width: height * 0.134,
-//             height: height * 0.134,
-//             child: Image.asset(
-//               'assets/images/muet logo.png',
-//             ),
-//           ),
-//         ),
-//       ),
-//       Expanded(
-//           child: Container(
-//               width: width,
-//               decoration: const BoxDecoration(
-//                 color: Colors.white,
-//                 borderRadius: BorderRadius.only(
-//                   topLeft: Radius.circular(50.0),
-//                   topRight: Radius.circular(50.0),
-//                 ),
-//               ),
-//               child:
-//                   Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-//                 Padding(
-//                   padding: EdgeInsets.only(
-//                       top: height * 0.032, bottom: height * 0.045),
-//                   child: Text(
-//                     textAlign: TextAlign.center,
-//                     title.toUpperCase(),
-//                     style: const TextStyle(
-//                         fontSize: 20,
-//                         color: Color(0xfffffc817),
-//                         letterSpacing: 2,
-//                         fontWeight: FontWeight.bold),
-//                   ),
-//                 )
-//               ])))
-//     ])
-//   ]));
-// }
+import 'package:flutter/material.dart';
+import '../utils/colors.dart';
+
+class CustomBackground extends StatelessWidget {
+  final Widget child;
+  final String title;
+
+  const CustomBackground({Key? key, required this.child, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: [
+        Container(
+          width: width,
+          height: height,
+          decoration: const BoxDecoration(
+            color: backgroundgrey,
+          ),
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                  top: height * 0.059, bottom: height * 0.035),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                  width: height * 0.134,
+                  height: height * 0.134,
+                  child: Image.asset(
+                    'assets/images/muet logo.png',
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: width,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50.0),
+                    topRight: Radius.circular(50.0),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: height * 0.032, bottom: height * 0.045),
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        title.toUpperCase(),
+                        style: const TextStyle(
+                            fontSize: 20,
+                            color: Color(0xfffffc817),
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Expanded(child: child),
+                  ],
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
